4d2d5f3184258f185da77eaa52be90c0c1d93861
closer2018 <425253153@qq.com>
2020-02-15 12:50:57 +0800

    2020-02-15 周六 12:50:57.75

--- git push pictures.bat
+++ git push pictures.bat
@@ -2,19 +2,19 @@
 
 cd G:\code\图床\picture
 echo "当前目录是：G:\code\图床\picture"
-echo "即将把代码提交到本地存储库..."
+echo "正在把代码提交到本地存储库... ..."
 git add *
 echo "add complete!"
 echo;
 
-echo "即将提交..."
+echo "正在提交... ..."
 set now=%date% %time%
 echo %now%
 git commit -m "%now%"
 echo "commit complete！"
 echo;
  
-echo "即将把更改提交到远程git服务器..."
+echo "正在把更改提交到远程git服务器... ..."
 git push origin master && git push giteeorigin master && git push codingorigin master
 echo;
  
